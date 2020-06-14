3f11760a69042b78583192bad4b18b6f71702a49
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2020-06-15 00:48:10 +0900

    지정 날짜 사이 주유량 구하기
    에러가 나긴하는데 값은 출력됨

--- resultBTW.js
+++ resultBTW.js
@@ -1,8 +1,8 @@
 const resultBtw = document.querySelector(".js-result-btw"),
   fromDate = document.querySelector(".js-from"),
   toDate = document.querySelector(".js-to");
-
-const range = [];
+ 
+  const obj=new Object()
 
 function findIndexTo(text) {
   const loadedData = localStorage.getItem(DATA_LS);
@@ -11,12 +11,11 @@ function findIndexTo(text) {
     return date.date === text; //이벤트핸들러로 받아온 날짜와 동일한 날짜를 가지고있는 객체를 불러온다
   }
   const foundDate = parsedData.find(findDate);
-  console.log(parsedData.indexOf(foundDate)); //같은날짜를 가진 객체의 인덱스
-  const objIndex = parsedData.indexOf(foundDate);
+  const objIndex = parsedData.indexOf(foundDate); //같은날짜를 가진 객체의 인덱스
   //해당 인덱스 오브젝트 저장
-  console.log(dataArr[objIndex]);
-  range.push(dataArr[objIndex]);
-  getResultFromDates(range);
+  //dataArr[objIndex]  객체로 저장
+  obj.to=dataArr[objIndex]
+  getResultFromDates(obj.to);
 }
 
 function findIndexFrom(text) {
@@ -25,22 +24,24 @@ function findIndexFrom(text) {
   function findDate(date) {
     return date.date === text;
   }
-  console.log(parsedData.find(findDate)); //from 날짜의 오브젝
-  const foundDate = parsedData.find(findDate);
-  console.log(parsedData.indexOf(foundDate)); //from 오브젝 인덱스
-  const objIndex = parsedData.indexOf(foundDate);
-  console.log(dataArr[objIndex]);
-  range.push(dataArr[objIndex])
-  getResultFromDates(range);
+  const foundDate = parsedData.find(findDate);//from 날짜의 오브젝
+  const objIndex = parsedData.indexOf(foundDate); //from 오브젝 인덱스
+ obj.from=dataArr[objIndex]
+  getResultFromDates(obj.from);
+
 }
 //++ 주행거리(km)/ 소모된 연료량(l) = 연비+++
 //( to 주행거리-from주행거리 )/ 소모 연료량(from-to 모든 연료량 더하기)
 
-function getResultFromDates(range) {
-  //index 0:from  1:to
-console.log(range)
-console.log(range.length)
-console.log(range[1].coast)
+function getResultFromDates() {
+  console.log(obj)
+  const previousFrom=obj.from;
+  const currentFrom=obj.to;
+  console.log(currentFrom, previousFrom)
+  console.log(currentFrom.totalTrip - previousFrom.totalTrip)
+  //previous~current까지 날짜별 주유량 다 더해야함??
+  
+  
 }
 
 function getDateTo(event) {
